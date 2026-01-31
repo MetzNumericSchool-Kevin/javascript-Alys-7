6f1e3aef617418b70b5f8a06c0f93a71f5c67865
Alys-7 <deprez.alais1@gmail.com>
2026-01-31 16:20:55 +0100

    exercices/07_affichage_potions.js

--- 1_Basics/exercices/07_affichage_potions.js
+++ 1_Basics/exercices/07_affichage_potions.js
@@ -2,3 +2,7 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+console.log(potions[0]);
+console.log(potions[potions.length - 1]);
+potions.forEach((nom_potion) => console.log(`Nous avons de la ${nom_potion} !`))
