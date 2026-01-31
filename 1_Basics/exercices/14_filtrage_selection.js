36183a7dd822c573462fab249ad57d8527700176
Alys-7 <deprez.alais1@gmail.com>
2026-01-31 17:40:34 +0100

    exercices/14_filtrage_selection.js

--- 1_Basics/exercices/14_filtrage_selection.js
+++ 1_Basics/exercices/14_filtrage_selection.js
@@ -8,3 +8,18 @@ const inventaire = [
   { nom: "Potion de force", prix: 60, stock: 3 },
   { nom: "Potion de vitesse", prix: 35, stock: 8 }
 ];
+
+const potions_en_stock = inventaire.filter((potion) => {
+  if (potion.stock > 0) {
+    return potion;
+  }
+});
+const potions_abordables = inventaire.filter((potion) => {
+  if (potion.prix < 40) {
+    return potion;
+  }
+});
+console.log(potions_en_stock.length);
+console.log(potions_abordables.length);
+console.log(potions_en_stock);
+console.log(potions_abordables);
