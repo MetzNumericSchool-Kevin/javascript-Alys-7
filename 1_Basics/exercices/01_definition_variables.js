ba4934a7c0524e067282a198dfd1b640b78b540c
Alys-7 <deprez.alais1@gmail.com>
2026-01-31 15:37:53 +0100

    exercices/01_definition_variables.js

--- 1_Basics/exercices/01_definition_variables.js
+++ 1_Basics/exercices/01_definition_variables.js
@@ -1,1 +1,12 @@
 // Définition de variables
+
+const nom_boutique = "Boutique d'Archibald";
+let nb_potions_soin = 4;
+let prix_potion_soin = 8;
+let est_ouvert = true;
+
+if (est_ouvert) {
+    console.log("La " + nom_boutique + " est ouverte!\nNous vous proposons:\n- " + nb_potions_soin + " potions de soin à " + prix_potion_soin + " 🪙 chacune");
+} else {
+    console.log("La " + nom_boutique + " est fermée!\nRevenez plus tard")
+}
