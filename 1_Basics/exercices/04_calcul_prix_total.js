9b80bbfc879c0391b8347f44fe91a781f5f80467
Alys-7 <deprez.alais1@gmail.com>
2026-01-31 15:57:30 +0100

    exercices/04_calcul_prix_total.js

--- 1_Basics/exercices/04_calcul_prix_total.js
+++ 1_Basics/exercices/04_calcul_prix_total.js
@@ -2,3 +2,7 @@
 
 // Variables des exercices précédents (modifie selon tes valeurs)
 let prix_potion_soin = 50;
+
+const quantite_potion = Number.parseInt(prompt("Nombre de potions de soin souhaitées: "))
+const prix_total = prix_potion_soin * quantite_potion;
+console.log(`Prix de ${quantite_potion} potions de soins : ${prix_total} 🪙 mon cher Aventurier. 💸`)
