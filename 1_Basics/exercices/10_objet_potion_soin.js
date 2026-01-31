4d081eae9cf315354d4e743683021fa8cc10821d
Alys-7 <deprez.alais1@gmail.com>
2026-01-31 16:32:30 +0100

    exercices/10_objet_potion_soin.js

--- 1_Basics/exercices/10_objet_potion_soin.js
+++ 1_Basics/exercices/10_objet_potion_soin.js
@@ -1,1 +1,3 @@
 // Rangeons les informations de la potion de soin dans un objet 🧹
+const informations_potion = {"nom_potion": "Potion de soin", "prix_potion": 50, "stock_potion": 10};
+console.log(informations_potion);
