cec2a6ff88de8cd2ddd51b5325f0478899ebe004
Alys-7 <deprez.alais1@gmail.com>
2026-01-31 17:31:09 +0100

    exercices/13_grand_marchand.js

--- 1_Basics/exercices/13_grand_marchand.js
+++ 1_Basics/exercices/13_grand_marchand.js
@@ -7,3 +7,25 @@ const inventaire = [
   { nom: "Potion d'endurance", prix: 40, stock: 0 },
   { nom: "Potion de force", prix: 60, stock: 3 },
 ];
+
+let prix_max = 0;
+let stock_max = 0;
+let stock_positif = 0;
+let potion_prix_max;
+let potion_stock_max;
+inventaire.forEach(potion => {
+  if (potion.prix > prix_max) {
+    prix_max = potion.prix;
+    potion_prix_max = potion;
+  }
+  if (potion.stock > stock_max) {
+    stock_max = potion.stock;
+    potion_stock_max = potion;
+  }
+  if (potion.stock > 0) {
+    stock_positif += 1;
+  }
+});
+console.log(`La potion la plus chère est la ${potion_prix_max.nom}`);
+console.log(`La potion le plus en stock est la ${potion_stock_max.nom}`);
+console.log(`Le nombre de potions en stock est de ${stock_positif}`);
