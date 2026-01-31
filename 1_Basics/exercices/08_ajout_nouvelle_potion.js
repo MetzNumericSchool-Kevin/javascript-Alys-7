4e03346e66ad3c2334fe2851585f021d59e89fdc
Alys-7 <deprez.alais1@gmail.com>
2026-01-31 16:27:30 +0100

    exercices/08_ajout_nouvelle_potion.js

--- 1_Basics/exercices/08_ajout_nouvelle_potion.js
+++ 1_Basics/exercices/08_ajout_nouvelle_potion.js
@@ -2,3 +2,8 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+potions.push("Potion de force");
+console.log(potions);
+potions.pop();
+console.log(potions);
