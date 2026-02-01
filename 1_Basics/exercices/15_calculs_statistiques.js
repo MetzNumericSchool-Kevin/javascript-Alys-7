b0c4399c7a29c1786c5c2712e44bd0aaa8c22cb9
Alys-7 <deprez.alais1@gmail.com>
2026-02-01 14:45:04 +0100

    exercices/15_calculs_statistiques.js

--- 1_Basics/exercices/15_calculs_statistiques.js
+++ 1_Basics/exercices/15_calculs_statistiques.js
@@ -7,3 +7,15 @@ const inventaire = [
   { nom: "Potion d'endurance", prix: 40, stock: 7 },
   { nom: "Potion de force", prix: 60, stock: 3 }
 ];
+
+let valeur_totale = 0;
+let prix_total = 0;
+let stock_total = 0;
+inventaire.forEach(potion => {
+  valeur_totale += potion.prix * potion.stock;
+  prix_total += potion.prix;
+  stock_total += potion.stock;
+});
+console.log(`La valeur totale de l'inventaire est de ${valeur_totale} 🪙.`)
+console.log(`Le prix moyen de l'inventaire est de ${prix_total / 4} 🪙.`)
+console.log(`Le stock total de l'inventaire est de ${stock_total} potions.`)
